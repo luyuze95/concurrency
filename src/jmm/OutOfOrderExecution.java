8c078743ad382477dc9da46cb0c8412f74d8e8c0
luyuze <627668587@qq.com>
2020-06-23 22:22:50 +0800

    jmm 1

---
+++ src/jmm/OutOfOrderExecution.java
@@ -0,0 +1,61 @@
+package jmm;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 演示重排序的现象
+ */
+public class OutOfOrderExecution {
+
+    private static int x = 0, y = 0;
+    private static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+
+        for (; ; ) {
+            i++;
+            x = 0;
+            y = 0;
+            a = 0;
+            b = 0;
+            CountDownLatch latch = new CountDownLatch(1);
+
+            Thread one = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        latch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    a = 1;
+                    x = b;
+                }
+            });
+            Thread two = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        latch.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    b = 1;
+                    y = a;
+                }
+            });
+            one.start();
+            two.start();
+            latch.countDown();
+            one.join();
+            two.join();
+            if (x == 0 && y == 0) {
+                System.out.println("第" + i + "次：" + "x = " + x + " y = " + y);
+                break;
+            } else {
+                System.out.println("第" + i + "次：" + "x = " + x + " y = " + y);
+            }
+        }
+    }
+}
