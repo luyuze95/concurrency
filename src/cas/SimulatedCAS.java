c31769c25017dd4f21ab273158de27af9620e286
luyuze <627668587@qq.com>
2020-06-30 10:51:13 +0800

    cas

---
+++ src/cas/SimulatedCAS.java
@@ -0,0 +1,15 @@
+package cas;
+
+/**
+ * 模拟CAS
+ */
+public class SimulatedCAS {
+    private volatile int value;
+    public synchronized int compareAndSwap(int expectedValue, int newValue) {
+       int oldValue = value;
+       if (oldValue == expectedValue) {
+           value = newValue;
+       }
+       return oldValue;
+    }
+}
